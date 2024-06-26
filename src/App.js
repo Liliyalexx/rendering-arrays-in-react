f497196e74ed1cd80be717a676ac046389f84c92
LiliyaLexx <liliya.fedyurina@gmail.com>
2024-06-26 13:04:54 -0700

    rendering array created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import learnerData from './data/learners';
+import Learner from './components/Learner'
+
 
 function App() {
+  const [learners] = useState({ learners: learnerData });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    {learners.learners.map((learner, index) => (
+      <Learner key={index} learner={learner} />
+    ))}
+  </div>
   );
 }
 
